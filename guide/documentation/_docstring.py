8a18dfe99069b9e64964844d6880aab528d197d3
Vinícius Biavatti <vinicius.biavatti@create.pt>
2023-08-28 12:44:15 +0100

    docstring

--- guide/documentation/_docstring.py
+++ guide/documentation/_docstring.py
@@ -1,11 +1,27 @@
 """
 Docstring
 
-* PEP 257 is the convention for docstrings in Python
-* Appears after the definition of a function, method, class, or module
-* NOTE: Check _doctests.py too
-* References: https://google.github.io/styleguide/pyguide.html
-              https://www.python.org/dev/peps/pep-0257/
+* A docstring (documentation string) is a comment that describes Python
+  resources like (modules, classes, functions, methods and packages)
+* This comment is an example of a docstring
+* Is is surrounded by triple double quotes
+* The docstring comment appears at a specific location depending on the
+  resource:
+  * Modules: At the top of the file
+  * Classes: Immediately after the class definition
+  * Functions/methods: Immediately after the definition
+  * Packages: In the __init__.py file
+* The PEP-257 contains the definitions and the convention for docstrings in
+  Python
+* NOTE: The Python documentation is flexible and allows to use other formats.
+  We don't need to use all the sections shown in the examples below. The
+  purpose of this guide is to show the most common sections used in docstrings
+  and to provider a template to use in our projects
+* NOTE: Take a look at _doctests.py file to see how to use doctests in
+  docstrings
+* References:
+  * https://google.github.io/styleguide/pyguide.html
+  * https://www.python.org/dev/peps/pep-0257/
 """
 
 
@@ -14,27 +30,24 @@ Docstring
 ###############################################################################
 
 
-# Module docstring
-# * All modules must contain a docstring at the top of the file
-# * It can has some examples of code to show the purpose of it
+# Documenting a Module
+# * All modules can contain a docstring at the top of the file that describes
+#   the module
+# * NOTE: We don't need to use all the sections shown in the example below. It
+#   is not a requirement.
 """
-A one line summary of the module or program, terminated by a period.
-
-Leave one blank line.  The rest of this docstring should contain an
-overall description of the module or program.  Optionally, it may also
-contain a brief description of exported classes ad functions and/or usage
-examples.n
+Description of the module here
 
 Examples:
     Typical usage example:
-    foo = ClassFoo()
-    bar = foo.FunctionBar()
+        foo = ClassFoo()
+        bar = foo.FunctionBar()
 
 See Also:
-    math.pi: Example of module constant documentation.
+    math.pi: Example of module constant documentation
 
 Notes:
-    Some notes here.
+    Some notes here
 
 Authors:
     John Due: john.due@email.com
@@ -50,26 +63,26 @@ References:
 
 
 # Class docstring
-# * It can has a list of attributes/methods description
+# * All classes can contain a docstring after the class declaration
 class SampleClass:
     """
-    Summary of class here.
-
-    More optional class information...
+    Description of the class here
 
     Attributes:
-        likes_spam: A boolean indicating if we like SPAM or not.
-        eggs: An integer count of the eggs we have laid.
+        name: The name of the resource
 
     Examples:
         Sum two integer or float numbers and returns the result
-        add(1, 3) -> 4
+            add(1, 3) -> 4
 
     See Also:
-        math.pi: Example of module constant documentation.
+        math.pi: Example of module constant documentation
 
     Notes:
-        Some notes here.
+        Some notes here
+
+    Authors:
+        John Due: john.due@email.com
 
     References:
         Python website: https://www.python.org/
@@ -86,36 +99,41 @@ class SampleClass:
 
 
 # Functions/Methods
-# * Use sections to separate documentation for arguments, keyword-arguments,
-#   returns, yeilds, etc.
-def functions_and_methods(value, *values, **kwargs):
+# * All functions/methods can contain a docstring after the declaration
+# * NOTE: For functions, the self argument is not considered since it
+#   represents the reference to the class instance
+def add(self, arg, *args, arg2=3, **kwargs):
     """
-    Summary of function/methods here.
+    Function/Method description here
 
     Args:
-        value: Positional argument description with some information and with
-            break line for multi-line description.
-        *values: Arbitraty argument description.
-        **kwvalue: Keyword argument description.
+        self: The class instance reference
+        arg: Description about the argument 1
+        *args: Description about arbitrary arguments
+        arg2: Description about the argument 2 (Default 'w')
+        **kwargs: Description about arbitrary keyword arguments
 
     Returns:
-        Return type description.
+        Return type description
 
     Yields:
-        Return type description used when the function is a generator.
+        Return type description used when the function is a generator
 
     Raises:
-        IOError: Error description and some information.
+        IOError: Error description and some information
 
     Examples:
-        Sum two integer or float numbers and returns the result
-        add(1, 3) -> 4
+        Some example description here
+            add(1, 2, arg2=4, debug=True) -> ...
 
     See Also:
-        math.pi: Example of module constant documentation.
+        math.pi: Example of module constant documentation
 
     Notes:
-        Some notes here.
+        Some notes here
+
+    Authors:
+        John Due: john.due@email.com
 
     References:
         Python website: https://www.python.org/
@@ -128,11 +146,14 @@ def functions_and_methods(value, *values, **kwargs):
 
 
 ###############################################################################
-# Print docstring
+# Show the docstring
 ###############################################################################
 
 
-# Access the docstring of object
+# Showing the docstring of a Python resource
+# * The docstring of a Python resource is stored into the __doc__ attribute
+# * We can print the content of this attribute to show the docstring
+# * The example below prints the docstring of the sum function
 print(sum.__doc__)
 """
 Return the sum of a 'start' value (default: 0) plus an iterable of numbers
