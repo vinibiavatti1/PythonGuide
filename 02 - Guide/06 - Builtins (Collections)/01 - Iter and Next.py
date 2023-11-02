0f3bfca721fa6a99dfe1668cf5acfaef4938da61
Vinícius Biavatti <vinicius.biavatti@create.pt>
2023-11-02 10:24:55 +0000

    Enumerate

--- 02 - Guide/06 - Builtins (Collections)/01 - Iter and Next.py
+++ 02 - Guide/06 - Builtins (Collections)/01 - Iter and Next.py
@@ -1,117 +1,205 @@
 """
 Iterators
 
-* An iterator is an object that contains a countable number of values
-* Technically, in Python, an iterator is an object which implements the
-  iterator protocol, which consist of the methods __iter__() and __next__()
-* Lists, tuples, dictionaries, strings and sets are all iterable objects
+* An iterator is an object that contains a countable number of values, that can
+  be iterated upon, like lists, tuples, dictionaries, strings and sets
+* To define an object as an iterator, it must implement the `__iter__()` and
+  `__next__()` methods from the iterator protocol
+* To iterate over an iterator, you can use the for loop, or the methods below:
 
-iter(collection [, sentinel])
-* Run the iter __iter__ method from object and return a iterator
-* sentinel is used when the first parameter is a callable, and the value cant
-  be known. The sentinel is the indicated value to stop the interation
-
-next(iterator [, default])
-* Retrieve the next item from the iterator by calling its __next__() method. If
-  default is given, it is returned if the iterator is exhausted, otherwise
-  StopIteration is raised
-* If default is given and the iterator is exhausted, it is returned instead of
-  raising StopIteration
-
-Syntax: iter(iterable [, sentinel])
-        next(iterator [, default])
+###############################################################################
+Method                               Description
+###############################################################################
+iter(__iterable, __sentinel = None)  Return an iterator object
+next(__i, __default = None)          Return the next item from iterator
+###############################################################################
 """
 
 
-# Iter and next
-# * Tuple / list / set
-tpl = (1, 2, 3, 4)
-tpl_iter = iter(tpl)
-print(next(tpl_iter))   # 1
-print(next(tpl_iter))   # 2
-print(next(tpl_iter))   # 3
-print(next(tpl_iter))   # 4
-# print(next(tpl_iter)) # StopIteration
+###############################################################################
+# Iter and Next
+###############################################################################
 
 
-# Iter and next with default value
-# * Tuple / list / set
-lst = (1, 2, 3, 4)
-lst_iter = iter(lst)
-print(next(lst_iter))     # 1
-print(next(lst_iter))     # 2
-print(next(lst_iter))     # 3
-print(next(lst_iter))     # 4
-print(next(tpl_iter, 5))  # 5 (default value)
+# Creating an iterator
+# * To create an iterator we can use the `iter()` function
+# * The target object must implement the __iter__ and __next__ methods
+# * The `iter()` function will perform the __iter__ method
+x = [1, 2, 3]
+x_iter = iter(x)
 
 
-# Iter and next
-# * Dict
-dct = {'name': 'Vini', 'age': 26}
-dct_iter = iter(dct)
-print(next(dct_iter))   # name
-print(next(dct_iter))   # age
-# print(next(dct_iter)) # StopIteration
+# Iterating over an iterator
+# * To iterate over an iterator we can use the `next()` function
+# * The `next()` function will perform the __next__ method
+# * Note that the `next()` function will return a different value each time
+#   that it is called
+x = [1, 2, 3]
+x_iter = iter(x)
+print(next(x_iter))  # 1
+print(next(x_iter))  # 2
+print(next(x_iter))  # 3
 
 
-# Iter and next
-# * String
-txt = 'Vini'
-txt_iter = iter(txt)
-print(next(txt_iter))   # V
-print(next(txt_iter))   # i
-print(next(txt_iter))   # n
-print(next(txt_iter))   # i
-# print(next(txt_iter)) # StopIteration
+# Iterating over an iterator (with exception)
+# * When a iterator is over, the `next()` function will raise a StopIteration
+#   exception
+# * We can handle this case by using a try/except block
+# * To avoid the exception, we can pass a default value to the `next()`
+#   function (check the next example)
+x = [1, 2, 3]
+x_iter = iter(x)
+try:
+    print(next(x_iter))  # 1
+    print(next(x_iter))  # 2
+    print(next(x_iter))  # 3
+    print(next(x_iter))  # <- This will raise a StopIteration exception
+except StopIteration:
+    print('StopIteration raised')
+# StopIteration raised
 
 
-# Create iterator
-# * Implement __iter__ and __next__ methods to make it as iterable
-class MyList:
+# Iterating over an iterator (with default value)
+# * When using a default value, the `next()` function will return the default
+#   value when the iterator is over
+x = [1, 2, 3]
+x_iter = iter(x)
+default = None
+print(next(x_iter, default))  # 1
+print(next(x_iter, default))  # 2
+print(next(x_iter, default))  # 3
+print(next(x_iter, default))  # None (default value)
+print(next(x_iter, default))  # None (default value)
+
+
+###############################################################################
+# Iter and Next (Different Collections)
+###############################################################################
+
+
+# Iterating a list
+# * The list is an iterable object, so we can iterate over it
+x = [1, 2, 3]
+x_iter = iter(x)
+print(next(x_iter))  # 1
+print(next(x_iter))  # 2
+print(next(x_iter))  # 3
+
+
+# Iterating a tuple
+# * The tuple is an iterable object, so we can iterate over it
+x = (1, 2, 3)
+x_iter = iter(x)
+print(next(x_iter))  # 1
+print(next(x_iter))  # 2
+print(next(x_iter))  # 3
+
+
+# Iterating a set
+# * The set is an iterable object, so we can iterate over it
+# * Frozen sets are also iterable
+# * NOTE: Sets are not ordered, so the iteration order is not guaranteed
+x = {1, 2, 3}
+x_iter = iter(x)
+print(next(x_iter))  # 3
+print(next(x_iter))  # 1
+print(next(x_iter))  # 2
+
+
+# Iterating a dictionary
+# * for dictionaries, the iterator will return the keys of the dictionary
+x = {'n1': 1, 'n2': 2, 'n3': 3}
+x_iter = iter(x)
+print(next(x_iter))  # n1
+print(next(x_iter))  # n2
+print(next(x_iter))  # n3
+
+
+# Iterating a string
+# * A string can be iterated, returning each character of the string
+x = 'Hi!'
+x_iter = iter(x)
+print(next(x_iter))  # H
+print(next(x_iter))  # i
+print(next(x_iter))  # !
+
+
+###############################################################################
+# Iterator Sentinel
+###############################################################################
+
+
+# Defining a function
+# * The `iter()` function can receive a sentinel parameter
+# * The sentinel parameter is used to define a value that will stop the
+#   iteration
+# * For example, if we have a non-deterministic iterator (random numbers for
+#   example), we can use the sentinel to stop the iteration when a specific
+#   value is reached
+# * When using the sentinel parameter, the first parameter must be a callable,
+#   that will be called each time that the `next()` function is called
+# * In this example, we will create a function that will return a random number
+def random_number():
+    from random import randint
+    return randint(1, 3)
+
+
+# Iterating with sentinel
+# * Knowing that the number can be between 1 and 3, we can set the sentinel to
+#   stop the iteration when the number 3 is reached
+# * Note that the function reference is passed as the first parameter since the
+#   iterator must be a callable when using the sentinel
+# * The example below is not deterministic, so the result may vary
+x_iter = iter(random_number, 3)
+try:
+    print(next(x_iter))  # 2
+    print(next(x_iter))  # 1
+    print(next(x_iter))  # <- This will raise a StopIteration exception
+except StopIteration:
+    print('StopIteration raised')
+# StopIteration raised
+
+
+###############################################################################
+# Iterator Protocol
+###############################################################################
+
+
+# Implementing the iterator protocol
+# * To implement the iterator protocol, we must implement the `__iter__()` and
+#   `__next__()` methods
+# * The `__iter__()` method must return the iterator object (a object that
+#   implements the `__next__()` method)
+# * The `__next__()` method must return the next value of the iterator
+# * Note that the state of the iterator is stored in the object itself, so we
+#   can have multiple iterators of the same object
+class CustomList:
+
     def __init__(self, *args):
-        self.list = args
+        self.__list = args
 
     def __iter__(self):
-        self.index = 0
+        self.__index = 0
         return self
 
     def __next__(self):
-        if self.index >= len(self.list):
+        if self.__index >= len(self.__list):
             raise StopIteration
-        current = self.list[self.index]
-        self.index += 1
+        current = self.__list[self.__index]
+        self.__index += 1
         return current
 
 
-my_list = MyList(1, 2, 3, 4)
-my_list_iter = iter(my_list)
-print(next(my_list_iter))   # 1
-print(next(my_list_iter))   # 2
-print(next(my_list_iter))   # 3
-print(next(my_list_iter))   # 4
-# print(next(my_list_iter)) # StopIteration
-
-
-# Using enumerate with custom iterator
-my_list = MyList(1, 2, 3, 4)
-for index, value in enumerate(my_list):
-    print(f'{index}: {value}', end=', ')
-    # 0: 1, 1: 2, 2: 3, 3: 4,
-print()
-
-
-# Iter with sentinel and next
-# * To use sentinel, the first parameter needs to be a callable (Function)
-i = 0
-
-
-def fn():
-    global i
-    i += 1
-    return i
-
-
-lst_iter = iter(fn, 3)
-print(next(lst_iter))     # 1
-print(next(lst_iter))     # 2
-# print(next(lst_iter))   # StopIteration
+# Iterating a custom object
+# * Now we have a custom object that implements the iterator protocol, we can
+#   iterate over it
+x = CustomList(1, 2, 3)
+x_iter = iter(x)
+try:
+    print(next(x_iter))  # 1
+    print(next(x_iter))  # 2
+    print(next(x_iter))  # 3
+    print(next(x_iter))  # <- This will raise a StopIteration exception
+except StopIteration:
+    print('StopIteration raised')
+# StopIteration raised
--- 02 - Guide/06 - Builtins (Collections)/02 - Enumerate.py
+++ 02 - Guide/06 - Builtins (Collections)/02 - Enumerate.py
@@ -1,54 +1,159 @@
 """
 Enumerate
 
-* When you use enumerate(), the function gives you back two loop variables
-  * The index of the current iteration
-  * The value of the item at the current iteration
-
-Syntax: enumerate(iterable [, start])
+* The `enumerate()` function transforms an iterable into a generator of tuples,
+  where each tuple contains the index and the value of the iterable
+* It is used to iterate over a iterable and get the index of the current
+  iteration
+* Basically, when we iterate over a collection using for-each, we don't have
+  the index of the current iteration, so we can use enumerate to get the index
+  of the current iteration when it is needed
+* Syntax: enumerate(iterable, start=0)
 """
 
 
-# Enumerate in for
-lst = ['a', 'b', 'c']
-for index, value in enumerate(lst):
-    print(f'{index}: {value}', end=', ')
-    # 0: a, 1: b, 2: c
-print()
+###############################################################################
+# Enumerate Result
+###############################################################################
 
 
-# Enumerate with start
-lst = ['a', 'b', 'c']
-for index, value in enumerate(lst, start=3):
-    print(f'{index}: {value}', end=', ')
-    # 3: a, 4: b, 5: c,
-print()
+# Adding indexes to a collection
+# * The result of the enumerate function is a generator of tuples, where each
+#   tuple contains the index and the value of the iterable
+# * Note that in this example, we needed to convert the result of the enumerate
+#   into a list since it is a generator
+x = ['a', 'b', 'c']
+x_enumerate = enumerate(x)
+y = list(x_enumerate)
+print(y)
+# [(0, 'a'), (1, 'b'), (2, 'c')]
 
 
-# Enumerate as iter
-lst = ['a', 'b', 'c']
-lst_enumerate = enumerate(lst)
-print(next(lst_enumerate))  # (0, 'a')
-print(next(lst_enumerate))  # (1, 'b')
-print(next(lst_enumerate))  # (2, 'c')
+# Adding indexes to a collection (with start)
+# * The `start` parameter is used to define the start index of the enumerate
+# * Note that in the example below, the index starts at 1 instead of 0
+x = ['a', 'b', 'c']
+x_enumerate = enumerate(x, start=1)
+y = list(x_enumerate)
+print(y)
+# [(1, 'a'), (2, 'b'), (3, 'c')]
 
 
-# Enumerate with list comprehension
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-lst_even = [value for index, value in enumerate(lst) if index % 2 == 0]
-print(lst_even)
-# [1, 3, 5, 7, 9]
+###############################################################################
+# Iterating Over an Enumerate
+###############################################################################
 
 
-# Create enumerate function
+# Iterating over an enumerate (with for-each) (XXX recommended XXX)
+# * When can use the for-each to iterate over the enumerate
+# * Since each item of the enumerate is a tuple, we can use tuple unpacking to
+#   get the index and the value of the current iteration
+x = ['a', 'b', 'c']
+for index, value in enumerate(x):
+    print(index, value, sep=': ')
+# 0: a
+# 1: b
+# 2: c
+
+
+# Iterating over an enumerate (with comprehension)
+# * The enumerate can be used in a comprehension
+# * In the example below, we will not store the comprehension result into
+#   memory since we don't need it
+x = ['a', 'b', 'c']
+[print(index, value, sep=': ') for index, value in enumerate(x)]
+# 0: a
+# 1: b
+# 2: c
+
+
+# Iterating over an enumerate (with next)
+# * The `next()` function can be used to iterate over the enumerate
+# * Note that we don't need to convert the enumerate into a iterator with
+#   `iter()` since the enumerate is already an iterator
+x = ['a', 'b', 'c']
+x_enumerate = enumerate(x)
+try:
+    print(next(x_enumerate))  # (0, 'a')
+    print(next(x_enumerate))  # (1, 'b')
+    print(next(x_enumerate))  # (2, 'c')
+    print(next(x_enumerate))  # <- This will raise a StopIteration exception
+except StopIteration:
+    print('StopIteration raised')
+# StopIteration raised
+
+
+###############################################################################
+# Using Enumerate in a Custom Collection
+###############################################################################
+
+
+# Creating a custom collection
+# * Any iterable object can be used with the enumerate function
+# * An iterable object is an object that implements the iterator protocol
+# * In the example below, we will create a custom collection that implements
+#   the `__iter__()` and `__next__()` methods
+class CustomList:
+
+    def __init__(self, *args):
+        self.__list = args
+
+    def __iter__(self):
+        self.__index = 0
+        return self
+
+    def __next__(self):
+        if self.__index >= len(self.__list):
+            raise StopIteration
+        current = self.__list[self.__index]
+        self.__index += 1
+        return current
+
+
+# Getting the enumerate result of a custom collection
+# * Knowing that the custom collection implements the iterator protocol, we can
+#   use the enumerate function to iterate over it
+x = CustomList('a', 'b', 'c')
+x_enumerate = enumerate(x)
+y = list(x_enumerate)
+print(y)
+# [(0, 'a'), (1, 'b'), (2, 'c')]
+
+
+# Iterating over a custom collection
+# * We can also iterate over the custom collection since it implements the
+#   iterator protocol
+x = CustomList('a', 'b', 'c')
+for index, value in enumerate(x):
+    print(index, value, sep=': ')
+# 0: a
+# 1: b
+# 2: c
+
+
+###############################################################################
+# How Enumerate Works
+###############################################################################
+
+
+# Creating a custom enumerate function
+# * The enumerate function can be implemented by using the `yield` keyword to
+#   create a generator
+# * In the example below, we will create a custom enumerate function that
+#   returns a generator of tuples
 def my_enumerate(iterable, start=0):
+    index = start
     for value in iterable:
-        yield start, value
-        start += 1
+        yield index, value
+        index += 1
 
 
-lst = ['a', 'b', 'c']
-for index, value in my_enumerate(lst):
-    print(f'{index}: {value}', end=', ')
-    # 0: a, 1: b, 2: c
-print()
+# Using the custom enumerate function
+# * We can now use the custom enumerate function to iterate over a collection
+#   as we would do with the built-in enumerate function
+x = ['a', 'b', 'c']
+for index, value in my_enumerate(x):
+    print(index, value, sep=': ')
+# 0: a
+# 1: b
+# 2: c
