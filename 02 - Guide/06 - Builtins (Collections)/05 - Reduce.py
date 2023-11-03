559cebedf0575237af79c8836719cc91032e0797
Vinícius Biavatti <vinicius.biavatti@create.pt>
2023-11-03 18:01:19 +0000

    Reduce and Sorted

--- 02 - Guide/06 - Builtins (Collections)/05 - Reduce.py
+++ 02 - Guide/06 - Builtins (Collections)/05 - Reduce.py
@@ -1,43 +1,168 @@
 """
 Reduce
 
-* NOTE: The reduce() builtin function was removed from Python 3+. To use it you
-  must import from functools module
-* NOTE: Reduce function was removed because the Python Owner decided that this
-  function is illegible, and 99% of times you can use a for loop to solve some
-  reduce problem
-* Reduce is used to reduce the iterable to a single value, manipulating the
-  current value and the accumulator value
-* Reduce has two parameters:
-  * accumulator
-  * current
-
-* Syntax
-  * reduce(function, iterable)
+* The reduce is a process of generating a single value from an iterable, for
+  example, sum all values from a list
+* We can use the `functools.reduce()` function to reduce an iterable to a
+  single value, but as the Python Community decided that this function is
+  illegible, its is recommended to use the builtin functions or a normal for
+  loop
+* To use the `reduce()` function it must be imported from the `functools`
+  module
+* For simple operations, we can use the builtin reduce functions: `sum()`,
+  `max()`, `min()`, `all()` and `any()`
+* A comprehension cannot be used to reduce an iterable to a single value since
+  it returns always a collection
+* The `functools.reduce()` function has the parameters below:
+  * function: The function that will be applied to every item of the iterable
+  * iterable: The iterable that will be used to apply the function
+  * initial: The initial value of the accumulator
+* Syntax: reduce(function, iterable, initial = None)
 """
+
+
+###############################################################################
+# Functools Reduce
+###############################################################################
+
+
+# Importing the reduce function
+# * To start using the reduce function, we need to import it from the functools
 from functools import reduce
 
 
-# Sum values with def
-def sum(accumulator, current):
-    return accumulator + current
+# The reduce function
+# * The reduce function is used to reduce an iterable to a single value
+# * The reduce function has the parameters `function`, `iterable` and `initial`
+# * The `function` parameter is the function that will be applied to every item
+#   of the iterable. This function must have two parameters:
+#   * accumulator (a): The value that will be used to accumulate the result
+#   * current (c): The current value of the iterable
+# * In the example below, we will use a lambda function to sum all values of a
+#   list
+x = [1, 2, 3]
+y = reduce(lambda a, c: a + c, x)
+print(y)
+# 6
 
 
-lst = [5, 2, 7, 8, 3, 5]
-result = reduce(sum, lst)
-print(result)
-# 30
+# The reduce function (with initial value)
+# * We can set an initial value to the accumulator using the `initial`
+#   parameter
+x = [1, 2, 3]
+y = reduce(lambda a, c: a + c, x, 10)
+print(y)
+# 16
 
 
-# Sum values with lambda
-lst = [5, 2, 7, 8, 3, 5]
-result = reduce(lambda a, c: a + c, lst)
-print(result)
-# 30
+###############################################################################
+# Builtin Reduce Functions
+###############################################################################
 
 
-# Get the max
-lst = [5, 2, 7, 8, 3, 5]
-result = reduce(lambda a, c: c if c > a else a, lst)
-print(result)
-# 8
+# Sum
+# * The `sum()` function is used to reduce an iterable to a single value by
+#   summing all values
+# * The second parameter is the initial value of the accumulator
+x = [1, 2, 3]
+y1 = sum(x)
+y2 = sum(x, 10)
+print(y1, y2)
+# 6 16
+
+
+# Max
+# * The `max()` function is used to reduce an iterable to a single value by
+#   getting the max value
+# * The second parameter is the key function, used to determine how to get the
+#   element from the iterable
+# * Note that the second collection is a dictionary, in which we must set the
+#   key function to define that the value of the dictionary will be used to
+#   compare the values
+x1 = [1, 2, 3]
+x2 = {'n1': 1, 'n2': 3, 'n3': 2}
+y1 = max(x1)
+y2 = max(x2, key=lambda k: x2[k])
+print(y1, y2)
+# 3 n2
+
+
+# Min
+# * The same as the `max()` function, but it gets the min value
+x1 = [1, 2, 3]
+x2 = {'n1': 1, 'n2': 3, 'n3': 2}
+y1 = min(x1)
+y2 = min(x2, key=lambda k: x2[k])
+print(y1, y2)
+# 1 n1
+
+
+# All
+# * The `all()` function is used to reduce an iterable to a single value by
+#   checking if all values are True
+# * For each element the "truth test procedure" is applied
+# * If all elements in the collection is considered True, the result will be
+#   True, otherwise, the result will be False
+# * Note that in the example below, the "x2" collection has a 0 value, that is
+#   considered False
+x1 = [1, 2, 3]
+x2 = [1, 2, 0]
+y1 = all(x1)
+y2 = all(x2)
+print(y1, y2)
+# True False
+
+
+# Any
+# * Same as the `all()` function, but it checks if any value is True, i.e. if
+#   at least one value is True
+# * If all values are False, the result will be False, otherwise, the result
+#   will be True
+# * Note that in the example below, the "x2" collection has the 1 value, that
+#   is considered True
+x1 = [0, 0, 0]
+x2 = [0, 0, 1]
+y1 = any(x1)
+y2 = any(x2)
+print(y1, y2)
+# False True
+
+
+###############################################################################
+# Reduce Operations
+###############################################################################
+
+
+# Reduce a collection (with builtin functions) (XXX recommended XXX)
+# * Using the builtin functions is the recommended way to do a reduce operation
+#   since they are more readable
+# * In the example below, we are reducing the collection by summing all values
+#   of the collection
+x = [1, 2, 3]
+y = sum(x)
+print(y)
+# 6
+
+
+# Reduce a collection (with for-each)
+# * We can use a for-each to reduce a collection when the builtin functions
+#   cannot be used
+# * It is recommended instead of using the `reduce()` function since it is
+#   more readable
+# * In the example below, we are reducing the collection by summing all values
+#   using a for-each loop
+x = [1, 2, 3]
+y = 0
+for n in x:
+    y += n
+print(y)
+# 6
+
+
+# Reduce a collection (with functools reduce)
+# * We can use the `reduce()` function from the `functools` module to reduce a
+#   collection
+x = [1, 2, 3]
+y = reduce(lambda a, c: a + c, x)
+print(y)
+# 6
--- 02 - Guide/06 - Builtins (Collections)/06 - Sorted.py
+++ 02 - Guide/06 - Builtins (Collections)/06 - Sorted.py
@@ -1,43 +1,119 @@
 """
-Reduce
+Sorted
 
-* NOTE: The reduce() builtin function was removed from Python 3+. To use it you
-  must import from functools module
-* NOTE: Reduce function was removed because the Python Owner decided that this
-  function is illegible, and 99% of times you can use a for loop to solve some
-  reduce problem
-* Reduce is used to reduce the iterable to a single value, manipulating the
-  current value and the accumulator value
-* Reduce has two parameters:
-  * accumulator
-  * current
-
-* Syntax
-  * reduce(function, iterable)
+* The `sorted()` builtin function is used to sort iterables
+* Different then the `sort()` method from the list class, the `sorted()`
+  function will return a new list with the sorted values, instead of sorting
+  the list itself
+* Consider using the `list.sort()` method when a big list is being sorted, to
+  avoid using unnecessary memory
+* The `sorted()` has three parameters:
+  * iterable: The iterable to be sorted
+  * key: A function to identify the key to be used to sort the iterable
+  * reverse: If the iterable should be sorted in reverse order
+* Syntax: sorted(iterable, key=None, reverse=False)
 """
-from functools import reduce
 
 
-# Sum values with def
-def sum(accumulator, current):
-    return accumulator + current
+###############################################################################
+# Sorted Operations
+###############################################################################
 
 
-lst = [5, 2, 7, 8, 3, 5]
-result = reduce(sum, lst)
-print(result)
-# 30
+# Sorting a collection
+# * The `sorted()` function will return a new list with the sorted values
+x = [3, 1, 3, 2]
+y = sorted(x)
+print(y)
+# [1, 2, 3, 3]
 
 
-# Sum values with lambda
-lst = [5, 2, 7, 8, 3, 5]
-result = reduce(lambda a, c: a + c, lst)
-print(result)
-# 30
+# Sorting a collection (in reverse order)
+# * The `reverse` parameter can be used to sort the collection in reverse order
+x = [3, 1, 3, 2]
+y = sorted(x, reverse=True)
+print(y)
+# [3, 3, 2, 1]
 
 
-# Get the max
-lst = [5, 2, 7, 8, 3, 5]
-result = reduce(lambda a, c: c if c > a else a, lst)
-print(result)
-# 8
+# Sorting a collection (using a common key selector)
+# * The `key` parameter can be used to set a function that will select the
+#   correct key that will be used to sort the collection
+# * In the example below, we are sorting a list of strings by the length of
+#   each string
+x = ['xxxx', 'xxxxxxx', 'xx']
+y = sorted(x, key=len)
+print(y)
+# ['xx', 'xxxx', 'xxxxxxx']
+
+
+# Sorting a collection (using a custom key selector)
+# * Different from the previous example, we can use a custom function to select
+#   the key that will be used to sort the collection
+# * In this example, we will use a lambda function to select the second value
+#   in the tuples to sort the list
+x = [(2, 5), (3, 4), (1, 6)]
+y = sorted(x, key=lambda v: v[1])
+print(y)
+# [(3, 4), (2, 5), (1, 6)]
+
+
+###############################################################################
+# Sorted vs list.sort
+###############################################################################
+
+
+# Sorting a list (with sorted())
+# * When using the `sorted()` function, a new list will be created with the
+#   sorted values
+# * This is different from the `list.sort()` method, that will sort the list
+#   itself
+x = [3, 1, 3, 2]
+y = sorted(x)
+print(x, y)
+# [3, 1, 3, 2] [1, 2, 3, 3]
+
+
+# Sorting a list (with list.sort())
+# * When using the `list.sort()` method, the list itself will be sorted
+#   instead of creating a new list
+# * This is better when sorting a big list, because it will not use unnecessary
+#   memory to store both lists
+# * NOTE: Check the "List.py" file for more details about the `list.sort()`
+x = [3, 1, 3, 2]
+x.sort()
+print(x)
+# [1, 2, 3, 3]
+
+
+###############################################################################
+# Custom Comparisons (functools.cmp_to_key)
+###############################################################################
+
+
+# Creating a custom comparison function
+# * The `sorted()` function can be used with a custom comparison function
+# * The comparison function must receive two values and return:
+#   * -1: If the first value is less than the second
+#   * 0: If the first value is equal to the second
+#   * 1: If the first value is greater than the second
+# * In the example below, we are defining a custom comparison function to sort
+#   a list of strings by the length of each string
+def compare(a, b):
+    if len(a) < len(b):
+        return -1
+    elif len(a) > len(b):
+        return 1
+    return 0
+
+
+# Sorting a list (with custom comparison)
+# * To use a custom comparison, we must use the `functools.cmp_to_key()`
+#   function to convert the comparison function to a key function
+# * The `cmp_to_key()` function will receive the comparison function and return
+#   a key function that can be used to sort the list
+from functools import cmp_to_key
+x = ['xxxx', 'xxxxxxx', 'xx']
+y = sorted(x, key=cmp_to_key(compare))
+print(y)
+# ['xx', 'xxxx', 'xxxxxxx']
